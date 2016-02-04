47b6aa262177bee50da4f9985de0961ef5820053
Havoc Pennington <hp@pobox.com>
2016-02-04 11:16:51 -0500

    Add module to encrypt/decrypt strings
    Planning to use this for credentials.

---
+++ project/internal/crypto.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import, print_function
+
+import base64
+import hashlib
+import json
+
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+class CryptoError(Exception):
+    pass
+
+
+def _b64decode(s):
+    try:
+        return base64.b64decode(s)
+    except Exception as e:
+        # very unclear what all b64decode can throw, but one thing is
+        # binascii.Error("incorrect padding")
+        raise CryptoError("base64 decoding error: " + str(e))
+
+
+def _b64encode(s):
+    return base64.b64encode(s).decode('ascii')
+
+
+def _key_from_secret(secret):
+    m = hashlib.sha256()
+    m.update(secret.encode('utf-8'))
+    key = m.digest()
+    assert len(key) == 32
+    return key
+
+
+def encrypt_bytes(message, secret):
+    key = _key_from_secret(secret)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    encrypted = cipher.encrypt(message)
+    dumped = json.dumps(dict(iv=_b64encode(iv), cipher='AES-CFB', message=_b64encode(encrypted)))
+    single_string = _b64encode(dumped.encode('utf-8'))
+    return single_string
+
+
+def decrypt_bytes(package, secret):
+    json_string = _b64decode(package).decode('utf-8')
+    try:
+        loaded = json.loads(json_string)
+    except ValueError as e:
+        raise CryptoError("encrypted package had bad json: " + str(e))
+
+    if 'cipher' not in loaded or loaded['cipher'] != 'AES-CFB':
+        raise CryptoError("bad cipher in json")
+
+    if 'iv' not in loaded:
+        raise CryptoError("bad iv in json")
+
+    iv = _b64decode(loaded['iv'])
+
+    if len(iv) != AES.block_size:
+        raise CryptoError("bad iv length in json")
+
+    if 'message' not in loaded:
+        raise CryptoError("no message in json")
+
+    message = _b64decode(loaded['message'])
+
+    key = _key_from_secret(secret)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    decrypted = cipher.decrypt(message)
+    return decrypted
+
+
+def encrypt_string(message, secret):
+    return encrypt_bytes(message.encode('utf-8'), secret)
+
+
+def decrypt_string(package, secret):
+    decrypted = decrypt_bytes(package, secret)
+    try:
+        return decrypted.decode('utf-8')
+    except UnicodeDecodeError:
+        # bad UTF-8 in encrypted message, probably means wrong
+        # secret...  return an empty string for this rather than
+        # raising, since we can't consistently raise when the
+        # secret is wrong (sometimes we might happen to get valid
+        # utf-8)
+        return ""
---
+++ project/internal/test/test_crypto.py
@@ -0,0 +1,98 @@
+from __future__ import absolute_import, print_function
+
+import json
+import pytest
+
+import project.internal.crypto as crypto
+
+
+def test_round_trip():
+    message = "Hello World"
+    encrypted = crypto.encrypt_string(message, "bar")
+    decrypted = crypto.decrypt_string(encrypted, "bar")
+    assert message == decrypted
+
+
+def test_wrong_secret_fails():
+    message = "Hello World"
+    encrypted = crypto.encrypt_string(message, "bar")
+    decrypted = crypto.decrypt_string(encrypted, "foo")
+    assert message != decrypted
+
+
+def _modified_package(original, json_modifier):
+    json_string = crypto._b64decode(original).decode('utf-8')
+    loaded = json.loads(json_string)
+    json_modifier(loaded)
+    return crypto._b64encode(json.dumps(loaded).encode('utf-8'))
+
+
+def _mangled_package(original, iv=None, cipher=None, message=None):
+    def json_modifier(loaded):
+        if iv is not None:
+            loaded['iv'] = iv
+        if cipher is not None:
+            loaded['cipher'] = cipher
+        if message is not None:
+            loaded['message'] = message
+
+    return _modified_package(original, json_modifier)
+
+
+def _incomplete_package(original, remove_field):
+    def json_modifier(loaded):
+        del loaded[remove_field]
+
+    return _modified_package(original, json_modifier)
+
+
+def test_invalid_json():
+    with pytest.raises(crypto.CryptoError) as excinfo:
+        crypto.decrypt_string(crypto._b64encode("}".encode('utf-8')), "foo")
+    assert 'bad json' in repr(excinfo.value)
+
+
+def _check_modified_package(modifier, expected_in_exception):
+    encrypted = crypto.encrypt_string("foo", "bar")
+    with pytest.raises(crypto.CryptoError) as excinfo:
+        crypto.decrypt_string(modifier(encrypted), "bar")
+    assert expected_in_exception in repr(excinfo.value)
+
+
+def _check_package_missing_field(remove_field, expected_in_exception):
+    def modifier(encrypted):
+        return _incomplete_package(encrypted, remove_field)
+
+    _check_modified_package(modifier, expected_in_exception)
+
+
+def _check_package_mangled_field(mangled_field, mangled_value, expected_in_exception):
+    def modifier(encrypted):
+        kwargs = {mangled_field: mangled_value}
+        return _mangled_package(encrypted, **kwargs)
+
+    _check_modified_package(modifier, expected_in_exception)
+
+
+def test_missing_cipher():
+    _check_package_missing_field('cipher', 'bad cipher in json')
+
+
+def test_bad_cipher():
+    _check_package_mangled_field('cipher', 'NOPE', 'bad cipher in json')
+
+
+def test_missing_iv():
+    _check_package_missing_field('iv', 'bad iv in json')
+
+
+def test_bad_iv():
+    _check_package_mangled_field('iv', 'NOPE', 'bad iv length in json')
+
+
+def test_bad_iv_base64():
+    _check_package_mangled_field('iv', 'NOPE1', 'base64 decoding error')
+
+
+def test_missing_message():
+    _check_package_missing_field('message', 'no message in json')
