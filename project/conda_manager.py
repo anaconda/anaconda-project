73210b3d29fff1ba65c956ba8c455e2fe5c650df
Havoc Pennington <hp@pobox.com>
2016-03-14 15:37:15 -0400

    Add pluggable CondaManager, fixes old issue 52

---
+++ project/conda_manager.py
@@ -0,0 +1,115 @@
+"""Abstract high-level interface to Conda."""
+from __future__ import absolute_import
+
+from abc import ABCMeta, abstractmethod
+
+from project.internal.metaclass import with_metaclass
+
+_conda_manager_classes = []
+
+
+def push_conda_manager_class(klass):
+    """Push the concrete subtype of ``CondaManager`` to be used."""
+    global _conda_manager_classes
+    assert issubclass(klass, CondaManager)
+    _conda_manager_classes.append(klass)
+
+
+def pop_conda_manager_class():
+    """Remove the most recently-pushed concrete subtype of ``CondaManager``."""
+    global _conda_manager_classes
+    assert len(_conda_manager_classes) > 0
+    _conda_manager_classes.pop()
+
+
+def new_conda_manager():
+    """Create a new concrete ``CondaManager``."""
+    global _conda_manager_classes
+    if len(_conda_manager_classes) == 0:
+        from project.internal.default_conda_manager import DefaultCondaManager
+        klass = DefaultCondaManager
+    else:
+        klass = _conda_manager_classes[-1]
+    return klass()
+
+
+class CondaManagerError(Exception):
+    """General Conda error."""
+
+    pass
+
+
+class CondaManager(with_metaclass(ABCMeta)):
+    """Methods for interacting with Conda.
+
+    This is meant to be a stateless class. Multiple may be created
+    and they may be used from multiple threads. If instances are
+    implemented using any global state under the hood, that global
+    state should be protected by locks, and shared among
+    ``CondaManager`` instances.
+
+    """
+
+    @abstractmethod
+    def find_environment_deviations(self, prefix, spec):
+        """Compute a ``CondaEnvironmentDeviations`` describing deviations of the env at prefix from the spec.
+
+        Raised exceptions that are user-interesting conda problems
+        should be subtypes of ``CondaManagerError``.
+
+        Args:
+            prefix (str): the environment prefix (absolute path)
+            spec (CondaEnvironment): specification for the environment
+
+        Returns:
+            a ``CondaEnvironmentDeviations`` instance
+
+        """
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def fix_environment_deviations(self, prefix, spec, deviations=None):
+        """Fix deviations of the env in prefix from the spec.
+
+        Raised exceptions that are user-interesting conda problems
+        should be subtypes of ``CondaManagerError``.
+
+        Args:
+            prefix (str): the environment prefix (absolute path)
+            spec (CondaEnvironment): specification for the environment
+            deviations (CondaEnvironmentDeviations): optional previous result from find_environment_deviations()
+
+        Returns:
+            None
+        """
+        pass  # pragma: no cover
+
+
+class CondaEnvironmentDeviations(object):
+    """Represents differences between actual and desired environment state."""
+
+    def __init__(self, summary, missing_packages, wrong_version_packages):
+        """Construct a ``CondaEnvironmentDeviations``."""
+        self._summary = summary
+        self._missing_packages = tuple(missing_packages)
+        self._wrong_version_packages = tuple(wrong_version_packages)
+
+    @property
+    def ok(self):
+        """True if no deviations were found, environment looks good."""
+        return len(self.missing_packages) == 0 and len(self.wrong_version_packages) == 0
+
+    @property
+    def summary(self):
+        """Summary description of status."""
+        return self._summary
+
+    @property
+    def missing_packages(self):
+        """Iterable collection of missing package names."""
+        return self._missing_packages
+
+    @property
+    def wrong_version_packages(self):
+        """Iterable collection of package names an unacceptable version installed."""
+        return self._wrong_version_packages
---
+++ project/test/test_conda_manager.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+from project.conda_manager import push_conda_manager_class, pop_conda_manager_class, new_conda_manager, CondaManager
+
+
+def test_use_non_default_conda_manager():
+    called = dict()
+
+    class MyCondaManager(CondaManager):
+        def find_environment_deviations(self, *args):
+            called['find_environment_deviations'] = args
+
+        def fix_environment_deviations(self, *args):
+            called['fix_environment_deviations'] = args
+
+    push_conda_manager_class(MyCondaManager)
+    try:
+        manager = new_conda_manager()
+        manager.find_environment_deviations(None, None)
+        manager.fix_environment_deviations(None, None)
+        assert dict(find_environment_deviations=(None, None), fix_environment_deviations=(None, None)) == called
+    finally:
+        pop_conda_manager_class()
