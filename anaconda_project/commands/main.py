8a8cb8153a36c1dc570b1da3519f9ab3393f26bd
Havoc Pennington <hp@pobox.com>
2016-04-13 10:27:02 -0400

    Refactor arg parser setup to be clearer about shared code
    set-variable should not have the --environment flag, remove it.
    
    The "common" args are the args for commands that do a prepare,
    so name them accordingly.

--- anaconda_project/commands/main.py
+++ anaconda_project/commands/main.py
@@ -26,18 +26,20 @@ def _parse_args_and_run_subcommand(argv):
 
     subparsers = parser.add_subparsers(help="Sub-commands")
 
-    def add_common_args(preset, with_ui=True):
+    def add_project_arg(preset):
         preset.add_argument('--project',
                             metavar='PROJECT_DIR',
                             default='.',
                             help="Project directory containing project.yml (defaults to current directory)")
+
+    def add_prepare_args(preset):
+        add_project_arg(preset)
+
         preset.add_argument('--environment',
                             metavar='ENVIRONMENT_NAME',
                             default=None,
                             action='store',
                             help="An environment name from project.yml")
-        if not with_ui:
-            return
         preset.add_argument('--mode',
                             metavar='MODE',
                             default=UI_MODE_TEXT_ASSUME_YES_DEVELOPMENT,
@@ -46,7 +48,7 @@ def _parse_args_and_run_subcommand(argv):
                             help="One of " + ", ".join(_all_ui_modes))
 
     preset = subparsers.add_parser('launch', help="Runs the project, setting up requirements first.")
-    add_common_args(preset)
+    add_prepare_args(preset)
     preset.add_argument('--command',
                         metavar="COMMAND_NAME",
                         default=None,
@@ -56,22 +58,22 @@ def _parse_args_and_run_subcommand(argv):
     preset.set_defaults(main=launch.main)
 
     preset = subparsers.add_parser('prepare', help="Sets up project requirements but does not run the project.")
-    add_common_args(preset)
+    add_prepare_args(preset)
     preset.set_defaults(main=prepare.main)
 
     preset = subparsers.add_parser('activate',
                                    help="Sets up project and outputs shell export commands reflecting the setup.")
-    add_common_args(preset)
+    add_prepare_args(preset)
     preset.set_defaults(main=activate.main)
 
     preset = subparsers.add_parser('set-variable',
                                    help="Set an environment variable and adds it to project if not present")
     preset.add_argument('vars_to_set', metavar='VARS_TO_SET', default=None, nargs=REMAINDER)
-    add_common_args(preset, with_ui=False)
+    add_project_arg(preset)
     preset.set_defaults(main=variable_commands.main, action="set")
 
     preset = subparsers.add_parser('unset-variable', help="Unset an environment variable and removes it from project")
-    add_common_args(preset, with_ui=False)
+    add_project_arg(preset)
     preset.add_argument('vars_to_unset', metavar='VARS_TO_UNSET', default=None, nargs=REMAINDER)
     preset.set_defaults(main=variable_commands.main, action="unset")
 
