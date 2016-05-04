e255077fc83faa1b89b490c15d387dda89100c63
Benjamin Zaitlen <quasiben@gmail.com>
2016-05-04 16:39:32 -0400

    retouching conda recipe

---
+++ RELEASE.md
@@ -0,0 +1,5 @@
+## Release
+
+```
+conda build conda.recipe
+```
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd "$RECIPE_DIR"/..
-"$PYTHON" setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
