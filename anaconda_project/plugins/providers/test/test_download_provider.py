8cdab3145248af62b33465e038bff80e1180525a
Rafael de Barros <rdebarros@continuum.io>
2016-05-05 14:17:51 -0500

    added checksum verification after download before unzip

--- anaconda_project/plugins/providers/test/test_download_provider.py
+++ anaconda_project/plugins/providers/test/test_download_provider.py
@@ -66,6 +66,7 @@ def test_prepare_download(monkeypatch):
             res.code = 200
             with open(os.path.join(dirname, 'data.csv'), 'w') as out:
                 out.write('data')
+            self._hash = '12345abcdef'
             raise gen.Return(res)
 
         def mock_checksum(self, fp):
@@ -78,6 +79,35 @@ def test_prepare_download(monkeypatch):
         result = prepare_without_interaction(project, environ=minimal_environ(PROJECT_DIR=dirname))
         assert hasattr(result, 'environ')
         assert 'DATAFILE' in result.environ
+
+    with_directory_contents({DEFAULT_PROJECT_FILENAME: DATAFILE_CONTENT}, provide_download)
+
+
+def test_prepare_download_mismatched_checksum_after_download(monkeypatch):
+    def provide_download(dirname):
+        @gen.coroutine
+        def mock_downloader_run(self, loop):
+            class Res:
+                pass
+
+            res = Res()
+            res.code = 200
+            with open(os.path.join(dirname, 'data.csv'), 'w') as out:
+                out.write('data')
+            self._hash = 'mismatched'
+            raise gen.Return(res)
+
+        def mock_checksum(self, fp):
+            return None
+
+        monkeypatch.setattr("anaconda_project.internal.http_client.FileDownloader.run", mock_downloader_run)
+        monkeypatch.setattr(
+            "anaconda_project.plugins.requirements.download.DownloadRequirement._checksum_error_or_none", mock_checksum)
+        project = project_no_dedicated_env(dirname)
+        result = prepare_without_interaction(project, environ=minimal_environ(PROJECT_DIR=dirname))
+        assert not result
+        assert ('Error downloading http://localhost/data.csv: mismatched hashes. '
+                'Expected: 12345abcdef, calculated: mismatched') in result.errors
 
     with_directory_contents({DEFAULT_PROJECT_FILENAME: DATAFILE_CONTENT}, provide_download)
 
