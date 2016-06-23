9714062b3ee922579625a161bafbb20297b48703
Havoc Pennington <hp@pobox.com>
2016-06-23 16:05:50 -0400

    Add build_and_upload.sh which builds and uploads packages

---
+++ build_and_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+# be sure we are in the right place
+test -e setup.py || exit 1
+test -d anaconda_project || exit 1
+
+(test -d build/packages && /bin/rm -r build/packages) || true
+python setup.py test
+python setup.py conda_package
+
+anaconda upload -u binstar --label dev build/packages/**/**/*.tar.bz2
