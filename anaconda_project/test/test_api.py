4544e95f2d12d96a078c5be268f04b87db557691
Havoc Pennington <hp@pobox.com>
2017-05-24 10:20:57 -0400

    In test_api.py use getfullargspec on py3 to avoid deprecation warnings

--- anaconda_project/test/test_api.py
+++ anaconda_project/test/test_api.py
@@ -10,11 +10,19 @@ import inspect
 
 from anaconda_project import api
 from anaconda_project import provide
+from anaconda_project.internal.py2_compat import _PY2
+
+if _PY2:
+    getargspec = inspect.getargspec
+else:
+    # py3 has getargspec also, but it causes a deprecation
+    # warning, so use getfullargspec.
+    getargspec = inspect.getfullargspec
 
 
 def _verify_args_match(function1, function2, ignored=('self', )):
-    f1spec = inspect.getargspec(function1)
-    f2spec = inspect.getargspec(function2)
+    f1spec = getargspec(function1)
+    f2spec = getargspec(function2)
     args1 = list(f1spec.args)
     args2 = list(f2spec.args)
     for param in ignored:
