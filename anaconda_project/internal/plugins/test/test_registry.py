f3cfec867467f67dc7619e2b2c135e991c3abdb7
fpliger <fabio.pliger@gmail.com>
2017-06-22 11:02:55 -0500

    assert first basic test for plugins scan function

--- anaconda_project/internal/plugins/test/test_registry.py
+++ anaconda_project/internal/plugins/test/test_registry.py
@@ -4,12 +4,22 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
-from anaconda_project.internal.plugins.registry import PluginRegistry
+from anaconda_project.internal.plugins import registry
+from unittest.mock import Mock
 
 
 def test_registry_init(tmpdir):
     plugins_dir = tmpdir.mkdir("test-temp").mkdir('anaconda-project-plugins')
     base_dir = plugins_dir.parts()[-2]
     paths = [base_dir.strpath]
-    registry = PluginRegistry(paths)
-    assert registry.search_paths == paths
+    plugins_registry = registry.PluginRegistry(paths)
+    assert plugins_registry.search_paths == paths
+
+
+def test_scan_paths(monkeypatch):
+    monkeypatch.setattr(registry.Plugin, 'create', Mock())
+    paths = ["a", "B", "c"]
+    plugins = registry.scan_paths(paths)
+
+    for arg in paths:
+        registry.Plugin.create.assert_any_call(arg)
