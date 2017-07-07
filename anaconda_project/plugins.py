fee23b7346102f49b3fc06e8531e44f7176cc33c
fpliger <fabio.pliger@gmail.com>
2017-07-07 18:07:32 -0500

    add plugins test and implementation

---
+++ anaconda_project/plugins.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © 2016, Continuum Analytics, Inc. All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+"""Plugins base classes and functions."""
+import os
+from os.path import join
+from anaconda_project.project_commands import (_ArgsTransformer, ProjectCommand, _http_specs)
+
+from entrypoints import get_group_named
+
+
+def _get_entry_points_plugins(entry_points_group):
+    """Return all the entry points plugins registered."""
+    return {name: plugin.load() 
+        for name, plugin in sorted(get_group_named(entry_points_group).items())
+    }
+
+
+def get_plugins(entry_points_group='anaconda_project.plugins'):
+    """Return all the entry points plugins registered.
+    
+    Inputs:
+    
+        - entry_points_group(str): name of the entry point group to look for
+            plugins. Default: 'anaconda_project.plugins'
+            
+    Output:
+        (dict) with plugin name as key and plugin generator function as value
+    
+    """
+    entry_point_plugins = _get_entry_points_plugins(entry_points_group)
+    return entry_point_plugins
+
+
+class ArgsTrasformerTemplate(_ArgsTransformer):
+    """Template class for plugins args trasformers.
+    Plugins args transformers should subclass it and redefine add_class
+    to implement custom arguments mapping.
+    """
+    def __init__(self, command, specs=None):
+        """Construct an ArgTransformer for the given command.
+
+        Args:
+            command (ProjectCommand): command that maps to the ArgsTransformer
+        """
+        # TODO: document specs
+        # TODO: decide the best way to let users define specs for their
+        #       transformers! Most likely specs should go on the command
+        #       itself?
+        super(ArgsTrasformerTemplate, self).__init__(_http_specs)
+        self.command = command
+
+    def add_args(self, results, args):
+        """Overwrite this method to add custom arguments transformation.
+        It should forwarding the arguments that are custom to the
+        specific command served by this trasformer (
+            i.e., '--anaconda-project-host' --> 'host'
+        )
+
+        Inputs:
+            - results [list(tuples)]: list of 2 element tuples the following:
+                           * option (str): name of the option
+                            * values (lst(str)): list of the values passed
+                                        for the option
+            - args [list]: list of the args already passed in
+
+        Returns:
+            (list) list of the transformed args (that should include args)
+        """
+        raise RuntimeError("not implemented")  # pragma: no cover
+
+
+class CommandTemplate(ProjectCommand):
+    """Represents a command from the project file."""
+
+    args_transformer_cls = None
+
+    def __init__(self, name, attributes):
+        """Construct a command with the given attributes.
+
+        Args:
+            name (str): name of the command
+            attributes (dict): named attributes of the command
+        """
+        super(CommandTemplate, self).__init__(name=name, attributes=attributes)
+        self._args_transformer = self.args_transformer_cls(self)
+
+    @property
+    def command_with_conda_prefix(self):
+        """Full command path pointing to <conda prefix>/bin."""
+        return join(os.environ['CONDA_PREFIX'], 'bin', self.command)
+
+    def _choose_args_and_shell(self, environ, extra_args=None):
+        """Prepare extra args calling class _args_trasform.transform_args."""
+        extra_args = self._args_transformer.transform_args(extra_args)
+        return self.choose_args_and_shell(environ, extra_args=extra_args)
+
+    def choose_args_and_shell(self, environ, extra_args=None):
+        """Overwrite this method to implement custom plugin logic."""
+        raise NotImplementedError()
---
+++ anaconda_project/test/test_plugins.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © 2016, Continuum Analytics, Inc. All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+import os
+from os.path import join
+
+from anaconda_project.test.environ_utils import minimal_environ
+from anaconda_project.test.project_utils import project_no_dedicated_env
+from anaconda_project.prepare import (prepare_without_interaction)
+from anaconda_project.internal.test.tmpfile_utils import (with_directory_contents_completing_project_file)
+from anaconda_project.project_file import DEFAULT_PROJECT_FILENAME
+from anaconda_project import project
+
+from anaconda_project.plugins import CommandTemplate, ArgsTrasformerTemplate
+
+def test_prepare_plugin_command(monkeypatch, tmpdir):
+    called_with = {}
+    cmd_name = 'custom-cmd'
+
+    def get_plugins_mock():
+        return {'valid_package_plugin': plugin_init_mock}
+
+    class TestTransformer(ArgsTrasformerTemplate):
+        def add_args(self, results, args):
+            return ['--show']
+
+    class TestCmd(CommandTemplate):
+        args_transformer_cls = TestTransformer
+        command = cmd_name
+
+        def choose_args_and_shell(self, environ, extra_args=None):
+            assert extra_args is None or isinstance(extra_args, list)
+
+            shell = False
+            args = [self.command_with_conda_prefix, 'custom-sub-cmd', '--%s.TESTARG' % self.command]
+
+            return args + extra_args, shell
+
+    def plugin_init_mock(*args, **kws):
+        called_with['args'] = args
+        called_with['kws'] = kws
+        return TestCmd(*args, **kws)
+
+    def check(dirname):
+        # monkeypatch.setattr(project.plugins_api, 'get_all', get_plugins_mock)
+        project.plugins_api.get_all = get_plugins_mock
+        _project = project_no_dedicated_env(dirname)
+        environ = minimal_environ()
+        result = prepare_without_interaction(_project, environ=environ, command_name='foo')
+
+        cmd_path = join(os.environ['CONDA_PREFIX'], 'bin', cmd_name)
+        expected = [cmd_path, 'custom-sub-cmd', '--%s.TESTARG' % cmd_name, '--show']
+        assert result.errors == []
+        assert result
+        assert result.command_exec_info.args == expected
+
+    with_directory_contents_completing_project_file(
+        {DEFAULT_PROJECT_FILENAME: """
+commands:
+    foo:
+       valid_package_plugin: foo.py
+packages:
+  - notebook
+""",
+         "foo.py": "# foo", }, check)
