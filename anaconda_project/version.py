35572c678991689d4098e232af9d55e33977e11d
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-09-21 14:24:23 -0400

    Add version file to source to avoid broken releases on github

---
+++ anaconda_project/version.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright © 2016, Continuum Analytics, Inc. All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+""""""
+
+VERSION_INFO = (0, 8, 0, 'dev0')
+__version__ = '.'.join(map(str, VERSION_INFO))
+version = __version__
