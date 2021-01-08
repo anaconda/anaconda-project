1b2568fb4e789650a908336e853e08d4a361e29a
Albert DeFusco <albert.defusco@me.com>
2021-01-08 13:25:50 -0500

    fix main cli tests

--- anaconda_project/internal/cli/test/test_main.py
+++ anaconda_project/internal/cli/test/test_main.py
@@ -14,7 +14,7 @@ import anaconda_project
 from anaconda_project.internal.cli.main import _parse_args_and_run_subcommand
 
 all_subcommands = ('init', 'run', 'prepare', 'clean', 'activate', 'archive', 'unarchive', 'upload', 'download',
-                   'add-variable', 'remove-variable', 'list-variables', 'set-variable', 'unset-variable',
+                   'dock', 'add-variable', 'remove-variable', 'list-variables', 'set-variable', 'unset-variable',
                    'add-download', 'remove-download', 'list-downloads', 'add-service', 'remove-service',
                    'list-services', 'add-env-spec', 'remove-env-spec', 'list-env-specs', 'export-env-spec', 'lock',
                    'unlock', 'update', 'add-packages', 'remove-packages', 'list-packages', 'add-platforms',
@@ -76,6 +76,7 @@ expected_usage_msg_format = (  # noqa
     '                        project files in it\n'
     '    upload              Upload the project to Anaconda Cloud\n'
     '    download            Download the project from Anaconda Cloud\n'
+    '    dock                Build a docker image of the Anaconda Project.\n'
     '    add-variable        Add a required environment variable to the project\n'
     '    remove-variable     Remove an environment variable from the project\n'
     '    list-variables      List all variables on the project\n'
