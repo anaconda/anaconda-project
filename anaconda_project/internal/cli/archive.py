31eef00fc95415522adf1dfff9f7d0b099469c54
Albert DeFusco <albert.defusco@me.com>
2021-04-13 15:41:25 -0400

    prepare all envs

--- anaconda_project/internal/cli/archive.py
+++ anaconda_project/internal/cli/archive.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import, print_function
 from anaconda_project.internal.cli.project_load import load_project
 from anaconda_project.internal.cli import console_utils
 import anaconda_project.project_ops as project_ops
+from anaconda_project.internal.cli.prepare import prepare_command
 
 
 def archive_command(project_dir, archive_filename, pack_envs):
@@ -19,7 +20,18 @@ def archive_command(project_dir, archive_filename, pack_envs):
     Returns:
         exit code
     """
+    if pack_envs:
+        prepare_status = prepare_command(project_dir,
+                                         ui_mode='production_defaults',
+                                         conda_environment=None,
+                                         command_name=None,
+                                         all=True)
+        if not prepare_status:
+            console_utils.print_status_errors(prepare_status)
+            return 1
+
     project = load_project(project_dir)
+
     status = project_ops.archive(project, archive_filename, pack_envs)
     if status:
         print(status.status_description)
