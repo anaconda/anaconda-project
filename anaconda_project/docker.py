c8cccaf82e0bdf94f5649747b63602aa304633d1
Albert DeFusco <albert.defusco@me.com>
2021-05-11 17:12:20 -0400

    lint

--- anaconda_project/docker.py
+++ anaconda_project/docker.py
@@ -15,7 +15,7 @@ DEFAULT_BUILDER_IMAGE = 'conda/s2i-anaconda-project-ubi8'
 
 try:
     FileNotFoundError  # noqa
-except NameError:      # pragma: no cover
+except NameError:  # pragma: no cover
     # python 2
     FileNotFoundError = OSError
 
