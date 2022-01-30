06c3c017d8b1859d19a2d367be7d0b3feb6cb551
Michael McNeil Forbes <michael.forbes+python@gmail.com>
2022-01-29 22:26:07 -0800

    BUG: Demonstrate packages!=dependencies (relevant for issue #337)

--- anaconda_project/test/conftest.py
+++ anaconda_project/test/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.fixture(params=["packages"])#, "dependencies"])
+@pytest.fixture(params=["packages", "dependencies"])
 def packages(request):
     """Ensure equivalence between `dependencies` and `packages`"""
     yield request.param
